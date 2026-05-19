5cb5b275897bbe9a4487dbdb47b9eacc8cdbc701
Larchenkov Alexander <larteasy@gmail.com>
2026-05-19 10:24:48 +0300

    Provided true path to nssm service name

--- Core/Services/ISleeper.cs
+++ Core/Services/ISleeper.cs
@@ -1,9 +1,14 @@
+using Microsoft.Extensions.Logging;
+
 namespace Core.Services;
 
 public interface ISleeper {
     void Sleep(TimeSpan duration);
 }
 
-public class Sleeper : ISleeper {
-    public void Sleep(TimeSpan duration) => Thread.Sleep(duration);
+public class Sleeper(ILogger<Sleeper> logger) : ISleeper {
+    public void Sleep(TimeSpan duration) {
+        logger.LogInformation("Sleeping for {Duration}", duration);
+        Thread.Sleep(duration);
+    }
 }
--- Core/Services/ServerService.cs
+++ Core/Services/ServerService.cs
@@ -3,8 +3,8 @@ using Microsoft.Extensions.Logging;
 
 namespace Core.Services;
 
-public class ServerService(IRconService rcon, INssmService nssm, IConfiguration config, ILogger<ServerService> logger) : IServerService {
-    private readonly string _serviceName = config["Server:ServiceName"]!;
+public class ServerService(IRconService rcon, INssmService nssm, IConfiguration config, ILogger<ServerService> logger, ISleeper sleeper) : IServerService {
+    private readonly string _serviceName = config["Nssm:Service"]!;
 
     public void Start() {
         logger.LogInformation("Starting server");
@@ -14,6 +14,7 @@ public class ServerService(IRconService rcon, INssmService nssm, IConfiguration 
     public void Stop() {
         logger.LogInformation("Stopping server");
         rcon.Stop();
+        sleeper.Sleep(TimeSpan.FromSeconds(30));
         nssm.Stop(_serviceName);
     }
 }
